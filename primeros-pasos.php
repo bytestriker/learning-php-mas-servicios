204b2805c86cdd1b80dc558dd28123661addbe54
Pedro <g33k.gu@gmail.com>
2021-04-07 14:14:26 -0500

    First steps
    Comments on code, hello world and first approaches to variables.

---
+++ primeros-pasos.php
@@ -0,0 +1,33 @@
+<?php 
+# comentarios: 
+// la doble diagonal y el # es un 'comentario' de código, el servidor ignora estas
+// líneas en tiempo de ejecución.
+
+// @author: Pedro Navarro Monter
+// @email: pnavarro@masservicios.com.mx
+// @date: Apr 7th 2021
+// @description: Ejercicio 1, hola mundo.
+
+print("Hola mundo"); # esta línea imprime hola mundo en la pantalla.
+
+// @todo: (indica tarea pendiente) Generar un print con mi nombre:
+print(' Pedro Navarro Monter');
+// Sobre las variables: 
+// un solo símbolo '=' permite asignar un valor a la variable.
+// nomenclatura de variables
+// $_variable;
+// $variable;
+// $Unavariable = 'texto';
+// $UnaVariable = 'dato';
+// $unaVariableMas;
+// $una_variable;
+// $una_variable_serpiente;
+
+// mi primer variable de texto
+$nombreCompleto = " Pedro Navarro Monter"; 
+print($nombreCompleto);
+// mi primer variable de números
+$edad = 0; // 
+print('$edad ');
+
+?>
